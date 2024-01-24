994dc627059b8d5f690511372b374679dbe2a2fa
Isabel Garrido <isabel.garrido@code.seat>
2024-01-24 19:35:45 +0100

    add new test and execute pitest

--- src/test/kotlin/com/isamadrid90/demo/GildedRoseTest.kt
+++ src/test/kotlin/com/isamadrid90/demo/GildedRoseTest.kt
@@ -14,52 +14,11 @@ class GildedRoseTest {
     }
 
     @Test
-    fun `items should decrease quality twice as fast when the sellIn is 0`() {
-        val itemList = mutableListOf(Item("Common", 0, 4))
-
+    fun `Aged Brie should decrease 1 sellIn`(){
+        val itemList = mutableListOf(Item("Aged Brie", 1, 2))
         GildedRose(itemList).updateQuality()
 
-        val expected = mutableListOf(Item("Common", -1, 2))
+        val expected = mutableListOf(Item("Aged Brie", 0, 3))
         assertEquals(expected, itemList)
-    }
-
-    @Test
-    fun `aged brie quality should increases with time`() {
-        val items = mutableListOf(Item("Aged Brie", 10, 20))
-
-        GildedRose(items).updateQuality()
-
-        val expected = mutableListOf(Item("Aged Brie", 9, 21))
-        assertEquals(expected, items)
-    }
-
-    @Test
-    fun `aged brie quality should not exceed 50`() {
-        val items = mutableListOf(Item("Aged Brie", 10, 50))
-
-        GildedRose(items).updateQuality()
-
-        val expected = mutableListOf(Item("Aged Brie", 9, 50))
-        assertEquals(expected, items)
-    }
-
-    @Test
-    fun `aged brie quality should be increased regularly when sellIn is 1`() {
-        val items = mutableListOf(Item("Aged Brie", 1, 33))
-
-        GildedRose(items).updateQuality()
-
-        val expected = mutableListOf(Item("Aged Brie", 0, 34))
-        assertEquals(expected, items)
-    }
-
-    @Test
-    fun `aged brie quality should increase twice as fast after sellIn`() {
-        val items = mutableListOf(Item("Aged Brie", 0, 20))
-
-        GildedRose(items).updateQuality()
-
-        val expected = mutableListOf(Item("Aged Brie", -1, 22))
-        assertEquals(expected, items)
     }
 }
